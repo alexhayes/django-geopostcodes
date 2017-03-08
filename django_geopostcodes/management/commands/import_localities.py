6008f6184c5c4f4feaf7da8e22469b3b1393a9dd
Alex Hayes <alex@alution.com>
2017-03-08 22:27:36 +1100

    Fixed issue in import_locations management command.

--- django_geopostcodes/management/commands/import_localities.py
+++ django_geopostcodes/management/commands/import_localities.py
@@ -8,9 +8,8 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import logging
-from optparse import make_option
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django_geopostcodes.helpers import import_localities
 from django.utils.translation import ugettext_lazy as _
 
@@ -22,20 +21,11 @@ class Command(BaseCommand):
     """
     Import a GeoPostCodes Locality file.
     """
-    args = 'infile'
-    help = _("Import a GeoPostCodes Locality file.")
-    option_list = BaseCommand.option_list + (
-        make_option('--delimiter',
-                    dest='delimiter',
-                    default=';',
-                    help=_("Set the CSV delimiter."),
-                    ),
-    )
+    help = _("Import a GeoPostCodes Places.csv file.")
+
+    def add_arguments(self, parser):
+        parser.add_argument('infile', type=str, help=_("Path to CSV file."))
+        parser.add_argument('--delimiter', type=str, default=';', help=_("Set the CSV delimiter."))
 
     def handle(self, *args, **options):
-        if len(args) != 1:
-            raise CommandError(_("Expected '%s' arguments.") % self.args)
-
-        import_localities(args[0], options['delimiter'])
-
-
+        import_localities(options['infile'], options['delimiter'])
