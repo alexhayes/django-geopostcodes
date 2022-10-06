e2ea325e53b3ad19685907bea70861c8715390ea
Alex Hayes <alex.hayes@rea-group.com>
2022-10-06 23:47:20 +1100

    Bump version: 0.2.2 → 0.3.0

--- django_geopostcodes/__init__.py
+++ django_geopostcodes/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Alex Hayes"""
 __email__ = 'alex@alution.com'
-__version__ = '0.2.2'
+__version__ = '0.3.0'
--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/alexhayes/django_geopostcodes',
-    version='0.2.2',
+    version='0.3.0',
     zip_safe=False,
 )
